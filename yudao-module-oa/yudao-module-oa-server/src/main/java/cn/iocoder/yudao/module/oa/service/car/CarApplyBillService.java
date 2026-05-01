24b11da9878f0a374a2c0018edad56d21cbc37d0
azimao <azimaoreed@gmail.com>
2026-05-01 23:12:40 +0800

    1.1.0 修复完毕后段自动部署版本

--- yudao-module-oa/yudao-module-oa-server/src/main/java/cn/iocoder/yudao/module/oa/service/car/CarApplyBillService.java
+++ yudao-module-oa/yudao-module-oa-server/src/main/java/cn/iocoder/yudao/module/oa/service/car/CarApplyBillService.java
@@ -15,7 +15,6 @@ public interface CarApplyBillService {
      * 保存用车申请单
      *
      * @param saveReqVO 保存信息
-     *
      * @return 编号
      */
     Long saveCarApplyBill(@Valid CarApplyBillSaveReqVO saveReqVO);
